75ee42f272f9427a30e035db8433364da2cf72bf
smartprecast.nb4@gmail.com <douglasaraujosampaio@gmail.com>
2021-05-13 17:27:07 -0300

    update to code sql

--- Backk/app/database/db.sql
+++ Backk/app/database/db.sql
@@ -11,13 +11,15 @@ numerorua int not null,
 bairro varchar(100) not null,
 cep varchar(100) not null,
 cidade varchar(100) not null,
-password varchar(40) not null
+password varchar(40) not null,
+constraint cpf unique (cpf)
 );
 
 create table cliente(
 id serial primary key,
 name varchar(40) not null,
-password varchar (40) not null
+password varchar (40) not null,
+constraint cliente_pkey primary key (id)
 );
 
 create table pesquisa(
@@ -27,5 +29,18 @@ datafim date not null,
 idcliente int,
 pergunta varchar(200) not null,
 resposta varchar(200),
-pessoaresp varchar(100)
+pessoaresp varchar(100),
+ constraint pesquisa_pkey primary key (id),
+    constraint "clientePesquisa" foreign key (idcliente)
+        references cliente (id)
 );
+
+CREATE TABLE pessoas_telefones
+(
+    id serial primary key,
+    telefone varchar(20) not null,
+    telfixo varchar(20) not null,
+    pessoa varchar(14) not null,
+    constraint pessoas_telefones_pkey primary key (id),
+    constraint "pessoaTelefones" foreign key (pessoa)
+        references pessoas (cpf)
